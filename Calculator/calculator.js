7d90fbed72ce3dc2c92915f34d078d25ca22f987
sallysway <ioanaandreea476@gmail.com>
2016-10-19 15:42:58 +0300

    calculator project

---
+++ Calculator/calculator.js
@@ -0,0 +1,76 @@
+﻿$(document).ready(function () {
+    var testNumLength = function (number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length - 9, 9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        }
+    };
+    var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+
+    $("#numbers a").not("#clear,#clearall").click(function () {
+        number += $(this).html();
+        totaldiv.text(number);
+        testNumLength(number);
+    }
+
+);
+    $("#operators a").not("#equals").click(function () {
+        operator = $(this).text();
+        newnumber = number;
+        number = "";
+        totaldiv.text(0);
+    });
+
+    $("#clear,#clearall").click(function () {
+        number = "";
+        totaldiv.text("0");
+        if ($(this).attr('id') == "#clearall") {
+            newnumber = "";
+        }
+    });
+
+    $("#equals").click(function () {
+        number = parseInt(number, 10);
+        newnumber = parseInt(newnumber);
+
+        if (operator == "+") {
+            var total = newnumber + number;
+            total = total.toString(10);
+            totaldiv.text(total);
+            testNumLength(total);
+            number = "";
+            newnumber = "";
+        }
+        else if (operator == '-') {
+            var total = newnumber - number;
+            total = total.toString(10);
+            totaldiv.text(total);
+            testNumLength(total);
+            number = "";
+            newnumber = "";
+        }
+        else if (operator == '*') {
+            var total = newnumber * number;
+            total = total.toString(10);
+            totaldiv.text(total);
+            testNumLength(total);
+            number = "";
+            newnumber = "";
+        }
+        else if (operator == '/') {
+            var total = newnumber / number;
+            total = total.toString(10);
+            totaldiv.text(total);
+            testNumLength(total);
+            number = "";
+            newnumber = "";
+        }
+    });
+});
---
+++ Calculator/oldCalculator.js
@@ -0,0 +1,40 @@
+﻿
+
+//var keys = document.getElementsByClassName("keys");
+var keys = document.querySelectorAll('#calculator span');
+// this returns an array like object with the operators
+var operators = document.getElementsByClassName("operator");
+
+console.log(operator);
+
+for (var x = 0; x < keys.length; x++) {
+    keys[x].onclick(function (e) {
+        var valueEntered = document.querySelectorAll('.screen');
+        var input = valueEntered.innerHTML;
+        var buttonInput = this.innerHTML;
+        var decimalAdded = false;
+
+        if (buttonInput = 'C') {
+            valueEntered.innerHTML = '';
+            var decimalAdded = false;
+        }
+
+        else if (buttonInput == '=') {
+            var calculation = input;
+            var lastNumber = calculation[calculation.length - 1];
+
+            if (operators.indexOf(lastChar) > -1 || lastChar == '.') {
+                calculation = calculation.replace(/.$/, '');
+            }
+
+            if (calculation) {
+                valueEntered.innerHTML = total(calculation);
+                decimalAdded = false;
+            }
+
+
+        }
+    })
+}
+
+
